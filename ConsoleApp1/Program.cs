f092047b60a8138b7cb029c6cbc6a07ade7ea98b
242407 <242407@DESKTOP-UV5ICI4>
2026-02-06 12:36:08 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                //свловарь перевода оценок
+                Dictionary<int, string> gradesDict = new Dictionary<int, string>
+                {
+                    {5, "отлично"},
+                    {4, "хорошо"},
+                    {3, "удовлетворительно"},
+                    {2, "неудовлетворительно"}
+                };
+
+                //ввод оценок
+                Console.Write("Введите оценки через пробел: ");
+                string input = Console.ReadLine();
+                string[] gradesInput = input.Split();
+
+                Console.WriteLine("\nРезультаты перевода:");
+                List<int> numericGrades = new List<int>();
+
+            foreach (string gradeStr in gradesInput)
+            {
+                if (int.TryParse(gradeStr, out int grade))
+                {
+                    if (gradesDict.ContainsKey(grade))
+                    {
+                        Console.WriteLine($"{grade} -> {gradesDict[grade]}");
+                        numericGrades.Add(grade);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{grade} -> нет такой оценки");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{gradeStr} -> некорректный ввод");
+                }
+            }
+        }
+    }   
+}
